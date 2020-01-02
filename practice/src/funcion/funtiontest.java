67d6d1ad088e9a308abd4fed527cf7b00badb39a
ikukim <ikukim@naver.com>
2020-01-02 17:03:14 +0900

    com

---
+++ practice/src/funcion/funtiontest.java
@@ -0,0 +1,18 @@
+package funcion;
+
+public class funtiontest {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		
+		int sum=addnum(num1,num2);
+		System.out.println(sum);
+	}
+	
+	public static int addnum(int n1,int n2) {
+		int result=n1+n2;
+		return result;
+	}
+
+}
