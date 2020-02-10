f4ef54c25ea9191431ffbf5d281befed0a204e58
ikukim <ikukim@naver.com>
2020-02-10 17:12:21 +0900

    aa

--- board/src/main/java/com/spring/board/service/BoardService.java
+++ board/src/main/java/com/spring/board/service/BoardService.java
@@ -1,13 +1,18 @@
 package com.spring.board.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.board.common.PagingUtil;
+import com.spring.board.common.ResultUtil;
 import com.spring.board.dao.BoardDao;
 import com.spring.board.dto.BoardDto;
+import com.spring.board.dto.CommonDto;
 import com.spring.board.form.BoardForm;
+import com.spring.board.form.CommonForm;
 
 @Service
 public class BoardService {
@@ -16,10 +21,45 @@ public class BoardService {
 	private BoardDao boardDao;
 
 	/** 게시판 - 목록 조회 */
-	public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {
+	public ResultUtil getBoardList(BoardForm boardForm) throws Exception {
 
-		return boardDao.getBoardList(boardForm);
+		ResultUtil resultUtil = new ResultUtil();
+
+		CommonDto commonDto = new CommonDto();
+
+		int totalCount = boardDao.getBoardCnt(boardForm);
+		if (totalCount != 0) {
+			CommonForm commonForm = new CommonForm();
+			commonForm.setFunction_name(boardForm.getFunction_name());
+			commonForm.setCurrent_page_no(boardForm.getCurrent_page_no());
+			commonForm.setCount_per_page(10);
+			commonForm.setCount_per_list(10);
+			commonForm.setTatal_list_count(totalCount);
+			commonDto = PagingUtil.setPageUtil(commonForm);
+		}
+
+		boardForm.setLimit(commonDto.getLimit());
+		boardForm.setOffset(commonDto.getOffset());
+
+		List<BoardDto> list = boardDao.getBoardList(boardForm);
+
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("list", list);
+		resultMap.put("totalCount", totalCount);
+		resultMap.put("pagination", commonDto.getPagination());
+
+		resultUtil.setData(resultMap);
+		resultUtil.setState("SUCCESS");
+
+		return resultUtil;
 	}
+
+	/** 게시판 - 목록 조회 */
+	/*
+	 * public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {
+	 * 
+	 * return boardDao.getBoardList(boardForm); }
+	 */
 
 	/** 게시판 - 상세 조회 */
 	public BoardDto getBoardDetail(BoardForm boardForm) throws Exception {
@@ -28,16 +68,16 @@ public class BoardService {
 
 		String searchType = boardForm.getSearch_type();
 
-		if("S".equals(searchType)){
-			
+		if ("S".equals(searchType)) {
+
 			int updateCnt = boardDao.updateBoardHits(boardForm);
-		
+
 			if (updateCnt > 0) {
 				boardDto = boardDao.getBoardDetail(boardForm);
 			}
-			
+
 		} else {
-			
+
 			boardDto = boardDao.getBoardDetail(boardForm);
 		}
 
@@ -48,17 +88,28 @@ public class BoardService {
 	public BoardDto insertBoard(BoardForm boardForm) throws Exception {
 
 		BoardDto boardDto = new BoardDto();
-		 
-        int insertCnt = 0;
-        
-        insertCnt = boardDao.insertBoard(boardForm);
-        
- 
-        if (insertCnt > 0) {
-            boardDto.setResult("SUCCESS");
-        } else {
-            boardDto.setResult("FAIL");
-        }
+
+		int insertCnt = 0;
+
+		// for (int a = 0; a < 1527; a++) {
+		//
+		// boardForm.setBoard_subject("제목_" + a);
+		// boardForm.setBoard_content("내용_" + a);
+		// boardForm.setBoard_writer("작성자_" + a);
+		//
+		// insertCnt = boardDao.insertBoard(boardForm);
+		// }
+
+		insertCnt = boardDao.insertBoard(boardForm);
+
+		// insertCnt = boardDao.insertBoardFail(boardForm);
+
+		if (insertCnt > 0) {
+			boardDto.setResult("SUCCESS");
+		} else {
+			boardDto.setResult("FAIL");
+		}
+
 		return boardDto;
 	}
 
@@ -86,6 +137,33 @@ public class BoardService {
 		int deleteCnt = boardDao.updateBoard(boardForm);
 
 		if (deleteCnt > 0) {
+			boardDto.setResult("SUCCESS");
+		} else {
+			boardDto.setResult("FAIL");
+		}
+
+		return boardDto;
+	}
+
+	/** 게시판 - 답글 등록 */
+	public BoardDto insertBoardReply(BoardForm boardForm) throws Exception {
+
+		BoardDto boardDto = new BoardDto();
+
+		BoardDto boardReplayInfo = boardDao.getBoardReplyInfo(boardForm);
+
+		boardForm.setBoard_seq(boardReplayInfo.getBoard_seq());
+		boardForm.setBoard_re_lev(boardReplayInfo.getBoard_re_lev());
+		boardForm.setBoard_re_ref(boardReplayInfo.getBoard_re_ref());
+		boardForm.setBoard_re_seq(boardReplayInfo.getBoard_re_seq());
+		
+		int insertCnt = 0;
+		
+		insertCnt += boardDao.updateBoardReSeq(boardForm);
+		
+		insertCnt += boardDao.insertBoardReply(boardForm);
+
+		if (insertCnt > 0) {
 			boardDto.setResult("SUCCESS");
 		} else {
 			boardDto.setResult("FAIL");
